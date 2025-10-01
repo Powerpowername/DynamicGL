3382b7622e05f863a58cfac530ad429876db41d7
Powerpowername <1361581236@qq.com>
2025-10-01 10:55:19 +0800

    添加立方体绘制

--- include/Object/Cube.cpp
+++ include/Object/Cube.cpp
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -19,6 +19,7 @@ protected:
     GLuint _num_vertices;
     glm::vec3 _color;
     Shader *_cubeShader;
+
 public:
     void init(Shader *shader)
     {
@@ -64,7 +65,7 @@ public:
             position[i * 3 + 1] = indexed_position[index[i] * 3 + 1];
             position[i * 3 + 2] = indexed_position[index[i] * 3 + 2];
         }
-        //为每个顶点设置法向量，顶点着色器后会为每个片段的法向量进行线性插值
+        // 为每个顶点设置法向量，顶点着色器后会为每个片段的法向量进行线性插值
         GLfloat per_surface_normals[] = {
             0, 0, 1, 0, 0, 1, 0, 0, 1,
             0, 0, 1, 0, 0, 1, 0, 0, 1,
@@ -84,11 +85,89 @@ public:
         glBufferData(GL_ARRAY_BUFFER, sizeof(position), position, GL_STATIC_DRAW);
 
         GLuint id_pos = glGetAttribLocation(_sid, "position");
+        glEnableVertexAttribArray(id_pos);
+        glVertexAttribPointer(id_pos, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
+        _num_vertices = sizeof(position) / sizeof(GLfloat) / 3;
+        _num_indices = sizeof(index) / sizeof(GLuint);
 
+        glGenBuffers(1, &_vbo_sur_norm);
+        glBindBuffer(GL_ARRAY_BUFFER, _vbo_sur_norm);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(per_surface_normals), per_surface_normals, GL_STATIC_DRAW);
 
+        id_pos = glGetAttribLocation(_sid, "surface_normal");
+        glEnableVertexAttribArray(id_pos);
+        glVertexAttribPointer(id_pos, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
+        _color = glm::vec3(1.0f, 1.0f, 1.0f);
+
+        glBindVertexArray(0);
+    }
+
+    void Draw(glm::mat4 model, glm::mat4 view, glm::mat4 projection)
+    {
+        glUseProgram(_sid);
+        glBindVertexArray(_vao);
+
+        _cubeShader->setMat4("model", model);
+        _cubeShader->setMat4("view", view);
+        _cubeShader->setMat4("projection", projection);
+
+        glDrawArrays(GL_TRIANGLES, 0, _num_vertices);
+
+        //  unbind shader program and vao
+        glUseProgram(0);
+        glBindVertexArray(0);
+    }
+
+    void SetColor(glm::vec3 rgb)
+    {
+        _color = rgb;
+    }
+    void Draw()
+    {
+        glUseProgram(_sid);
+        glBindVertexArray(_vao);
+
+        _cubeShader->setVec3("light_position", _light_pos);
+        _cubeShader->setVec3("camera_position", _camera_pos);
+        _cubeShader->setVec3("shape_color", _color);
+        _cubeShader->setVec4("clip_coord", _clip_coord);
+
+        _cubeShader->setInt("shadow_mapping_effect", _shadow_mapping_effect);
+        _cubeShader->setInt("shadow_buffer_tex_size", _shadow_buffer_texture_width);
+        _cubeShader->setInt("window_width", _window_width);
+        _cubeShader->setInt("window_height", _window_height);
+
+        _cubeShader->setVec3("sun_dir", _sun_dir);
+        _cubeShader->setVec3("sun_col", _sun_col);
+
+        if (_has_shadow_buffer)
+        {
+            _cubeShader->setMat4("shadow_matrix", _shadow);
+
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, _shadow_tex_id);
+
+            _cubeShader->setInt("shadow_buffer_tex", 1);
+        }
+
+        Draw(_model, _view, _projection);
+
+        if (_has_shadow_buffer)
+        {
+            glBindTexture(GL_TEXTURE_2D, 0);
+        }
+
+        glUseProgram(0);
+        glBindVertexArray(0);
+    }
+
+    void cleanup()
+    {
+        glDeleteBuffers(1, &_vbo_pos);
+        glDeleteBuffers(1, &_vbo_sur_norm);
+        glDeleteVertexArrays(1, &_vao);
+        glDeleteProgram(_sid);
     }
 };
-
-
